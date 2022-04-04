366d7c248b3116d11c4165fc49293ed7dd726b56
Vlasta Rencova <rencovav@seznam.cz>
2022-04-04 13:44:43 +0200

    Add project files.

---
+++ square-pyramid-numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿    int sum = 0;
+    Console.Write("N : ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    for (int i = 1; i <= n; i++)
+    {
+        sum = 0;
+        for (int j = 1; j <= i; j++)
+        {
+            sum += j * j;
+            Console.Write(j + "^2");
+            if (j != i)
+            {
+                Console.Write(" + ");
+            }
+            else
+            {
+                Console.Write(" = ");
+            }
+        }
+        Console.Write(sum);
+        Console.WriteLine();
+    }
+    Console.ReadKey();
